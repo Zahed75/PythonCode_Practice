501aac635caf106c8f3f0130ff444d2e951e70df
zahed75 <zahedhasan.cs@gmail.com>
2021-02-09 14:21:42 +0600

    list done

---
+++ list.py
@@ -0,0 +1,15 @@
+# Course=["CISCO","MICROTIK","ANDROID","PYTHON","Django","Html5"]
+
+# print(Course[1:3])
+
+#check iteam
+Course=["CISCO","MICROTIK","ANDROID","PYTHON","Django","Html5"]
+print("python" in Course)
+
+#add item
+
+Course=["CISCO","MICROTIK","ANDROID","PYTHON","Django","Html5"]
+print(Course+["JS","Ajax"])
+
+Course=["CISCO","MICROTIK","ANDROID","PYTHON","Django","Html5"]
+print(Course*4)
---
+++ list_fun.py
@@ -0,0 +1,11 @@
+#list iteam add
+
+training=["WebDesign","Python","Django","Js"]
+training.append(("MICROTIK"))
+print(training)
+
+#to check list iteam length
+print(len(training))
+#add iteam in position
+training.insert(4,"CISCO")
+print(training)
