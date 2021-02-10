1a13ac6f1fa7e785a14fc8481a1eec6c1001bed2
zahed75 <zahedhasan.cs@gmail.com>
2021-02-10 13:25:43 +0600

    for done

--- for.py
+++ for.py
@@ -1,6 +1,21 @@
-name = ["Zahed","Hasan","Ayat","Jani na","ami","chini na ami"]
+'''name = ["Zahed","Hasan","Ayat","Jani na","ami","chini na ami"]
 print(name)
 count = 0
 while count<6:
     print(name[count])
-    count=count+1
+    count=count+1'''
+
+name = ["Zahed","Hasan","Ayat","Jani na","ami","chini na ami"]
+
+for x in name:
+    print(x)
+
+#--summation of all values
+
+num = [23,32,42,342,32,3]
+sum = 0
+
+for x in num:
+    sum=sum+x
+
+print(sum)
