a060e6df3a1053d287f02ef54227cde540987289
zahed75 <zahedhasan.cs@gmail.com>
2021-02-11 01:46:22 +0600

    list done here

--- list_numberInput.py
+++ list_numberInput.py
@@ -1,10 +1,31 @@
-number=input("Enter list of numbers:")
+# number=input("Enter list of numbers:")
 #10 20 30 40
-list= number.split()# 10,20,30
+'''list= number.split()# 10,20,30
 
 sum=0
 for x in list:
     sum= sum+int(x)
 print(sum)
+'''
 
+### List count 
 
+CountWords=0
+CountLetters=0
+CountDigits=0
+
+text=input("Please enter your values:")
+# list=text.split()
+
+for x in text:
+    x=x.lower()
+    if x>="a" and x<="z":
+        CountLetters=CountLetters+1
+    elif x>="0" and x<="9":
+        CountDigits=CountDigits+1
+    elif x ==" ":
+        CountWords=CountWords+1
+
+print(CountWords)
+print(CountLetters)
+print(CountDigits)
