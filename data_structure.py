4072c8708290517a5c1673d0bd774b7cee8c3ab8
zahed75 <zahedhasan.cs@gmail.com>
2021-02-13 20:13:41 +0600

    function done

---
+++ data_structure.py
@@ -0,0 +1,21 @@
+#Data_Structure 
+#push and pop
+#push mane data rakha ar pop mane data delete kora
+books=[]
+
+books.append("Python")
+books.append("Django")
+books.append("JavaScript")
+books.append("Html5")
+books.append("CSS3")
+books.pop()
+
+
+print(books)
+print("Now the top book is",books[-1])
+print("Now the top book is",books[-1])
+if not books:
+    print("Sob Sesh hoiya gese")
+
+
+#QUEE FIRST IN FIRST OUT
---
+++ function.py
@@ -0,0 +1,16 @@
+#there are 2 kinds of function
+# library and User defiend function
+
+def add(x,y):
+    sum=x+y
+    print(sum)
+
+add(10,20)
+add(10,20)
+add(10,20)
+
+def summation(x,y,z):
+    sum=x+y+z
+    print(sum)
+
+summation(20,10,40)
