fd27e1688313493a337a51d8e768be91cbae1e62
zahed75 <zahedhasan.cs@gmail.com>
2021-02-14 19:32:40 +0600

    class done

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-# class is a gropu of information where all kind info is
+# class is a group of information where all kind info is
 #  belongs into a class and all of the elemntes are belongs to object
 #class er under e sub-Class thkbe
 class student():
@@ -27,6 +27,8 @@ class family_members():
     School=""
     contact=""
     birth_date=""
+   
+
 
 
 Zara=family_members()
@@ -36,4 +38,32 @@ Zara.School="Wills Little Flowers"
 Zara.contact="0182919082"
 Zara.birth_date="19january"
 
+
+
 print(f"Name:{Zara.Name},B_group:{Zara.B_group},School:{Zara.School},Contact:{Zara.contact},birth_date:{Zara.birth_date}")
+
+#//////////////////////////////// class er under method use kora
+#Famliy in class
+
+class family_members():
+    Name=""
+    B_group=""
+    School=""
+    contact=""
+    birth_date=""
+    def display(self)://--nije nijeke call korte pare
+        print(f"\nName:{self.Name},B_group:{self.B_group},School:{self.School},Contact:{self.contact},birth_date:{self.birth_date}")
+
+
+
+
+Zara=family_members()
+Zara.Name="Zara Hasan"
+Zara.B_group="B+"
+Zara.School="Wills Little Flowers"
+Zara.contact="0182919082"
+Zara.birth_date="19january"
+
+Zara.display()
+
+# print(f"Name:{Zara.Name},B_group:{Zara.B_group},School:{Zara.School},Contact:{Zara.contact},birth_date:{Zara.birth_date}")
---
+++ constructor.py
@@ -0,0 +1,25 @@
+#construcotr is special types method jake alada vabe call dite hoy na
+class info():
+    name=""
+    roll=""
+
+    def __init__(self, name,roll):
+        self.name=name
+        self.roll=roll
+
+    def display(self):
+        print(f"Roll:{self.roll},Name:{self.name}")
+
+   
+
+zara=info("Zahed",555)
+
+zara.display()
+
+
+
+
+ 
+
+
+    
