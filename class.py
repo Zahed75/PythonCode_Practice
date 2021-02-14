68e5a633a3f85d61fd65525f758e899ee71628d8
zahed75 <zahedhasan.cs@gmail.com>
2021-02-14 18:28:58 +0600

    class created

---
+++ class.py
@@ -0,0 +1,39 @@
+# class is a gropu of information where all kind info is
+#  belongs into a class and all of the elemntes are belongs to object
+#class er under e sub-Class thkbe
+class student():
+    name=""
+    gpa=""
+
+
+Zara=student()
+Zara.name="Zara Hasan"
+Zara.gpa=3.53
+print(f"Name:{Zara.name},GPA{Zara.gpa}")
+
+#//////////////////////
+Zayan=student()
+Zayan.name="Zayan Hasan"
+Zayan.gpa=3.53
+print(f"Name:{Zayan.name},GPA{Zayan.gpa}")
+# print(isinstance(rahim,student))
+
+
+#Famliy in class
+
+class family_members():
+    Name=""
+    B_group=""
+    School=""
+    contact=""
+    birth_date=""
+
+
+Zara=family_members()
+Zara.Name="Zara Hasan"
+Zara.B_group="B+"
+Zara.School="Wills Little Flowers"
+Zara.contact="0182919082"
+Zara.birth_date="19january"
+
+print(f"Name:{Zara.Name},B_group:{Zara.B_group},School:{Zara.School},Contact:{Zara.contact},birth_date:{Zara.birth_date}")
